bec0061dff5f6b53c6334dc199078252f67247ef
HemingWu <jianmo_o@163.com>
2018-02-02 13:56:01 +0800

    init

--- src/main/java/top/joylife/postoffice/controller/TestController.java
+++ src/main/java/top/joylife/postoffice/controller/TestController.java
@@ -2,7 +2,6 @@ package top.joylife.postoffice.controller;
 
 import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.joylife.postoffice.dao.entity.Test;
