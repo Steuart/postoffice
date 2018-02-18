25b38e5ec88447b10771ca5dd70068b48d59192f
HemingWu <jianmo_o@163.com>
2018-02-18 12:00:21 +0800

    test

---
+++ src/main/java/top/joylife/postoffice/service/impl/SeedServiceImpl.java
@@ -0,0 +1,11 @@
+package top.joylife.postoffice.service.impl;
+
+import org.springframework.stereotype.Service;
+import top.joylife.postoffice.service.SeedService;
+
+/**
+ * created by wuhaiming on 2018/2/6
+ */
+@Service(value = "seedService")
+public class SeedServiceImpl implements SeedService {
+}
---
+++ src/test/java/top/joylife/postoffice/TaskExecutorTest.java
@@ -0,0 +1,16 @@
+package top.joylife.postoffice;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * created by wuhaiming on 2018/2/6
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskExecutorTest {
+
+
+}
